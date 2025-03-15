3aa5e67afcda8708a6f804c3cf8faa5091499fbe
Renda <Giovanni.Renda@computacenter.com>
2025-03-15 21:30:46 +0100

    project finished

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const display = document.getElementById("display");
+
+function addTodisplay(input){
+    display.value += input;
+}
+
+function cls(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    } catch {
+        display.value = "error";
+    }
+    
+}
