8240080a466c2c2d28e13348a099e2ded6c9a355
Giovanni <giovanni.renda16@gmail.com>
2025-03-16 10:33:32 +0100

    added functionalities

--- script.js
+++ script.js
@@ -11,10 +11,36 @@ function clearDisplay(){
 
 function calculate(){
     let expression = display.value.replace(/x/g, '*');
+    
+    if(expression.includes("%")){
+        percent(expression);
+    }
+    
     try{
         display.value = eval(expression);
     } catch {
         display.value = "error";
     }
     
+}
+
+function deleteLast() {
+    newStr = display.value.slice(0, -1);
+    display.value = newStr;
+}
+
+function percent(expr){
+    const arr = expr.split('%');
+    display.value = arr[0] / 100 * arr[1];
+}
+
+
+function invert() {
+    const arr = display.value.split("");
+    if(arr[0] !== "-"){
+        arr.unshift("-");
+    }  else {
+        arr.shift()
+    }
+    display.value = arr.join("");
 }
